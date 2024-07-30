beb3e7d36a8c70567c6d8d6f9698e8c60348a612
Ruchita Sagalgile <ruchita.sagalgile@erelego.com>
2024-07-30 17:27:41 +0530

    How JS Works & Execution Context

---
+++ JS Concepts/EP.1.js
@@ -0,0 +1,20 @@
+/**
+ * How JS Works & Execution Context:
+ *
+ * everything in js happens inside an execution context
+ *
+ * in the execution context all the variables and function are stored
+ * in the form of key value pair
+ *
+ * second component of execution context is code component
+ * here the code run line by line we are also called it as
+ * thread of execution
+ *
+ * javascript is synchronous single threaded launguage
+ *
+ * synchronous single threaded means one command at a time with a 
+ * specific order
+ * 
+ * 
+
+ */
