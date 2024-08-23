0aa563c5f91733847d6dd7c26a613573b235eba7
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-23 20:57:49 +0530

    simple basic questions on obj

--- OBJECT/app.js
+++ OBJECT/app.js
@@ -1,19 +1,104 @@
+//1 object property existence
+
+const obj = { a: 1, b: 2, c: 3 };
+console.log("a" in obj); //true
+console.log(obj.hasOwnProperty("b")); //true
+console.log(obj.hasOwnProperty("cd")); //true
+
+//2 accessing nested properties
 /**
- * 1program to swap two numbers without using third
- * variable in js
+ * You can use optional chaining (?.) to safely access
+ * nested properties. This prevents errors if any part
+ *  of the chain is undefined or null.
+ */
+
+const obj1 = { a: { b: { c: 3 } } };
+
+console.log(obj1?.a?.b?.c); //3
+
+/**
+ * 3 cloning an object
+ *You can use the Object.assign() method or the spread 
+ operator (...) to create a shallow copy of an object.
+ */
+
+const obj3 = { a: 1, b: 2 };
+
+//using Object.assign
+const copy1 = Object.assign({}, obj3);
+console.log(copy1);
+
+//using spread operator
+let copy2 = { ...obj3 };
+console.log(copy2);
+
+/**
+ * 4 Iterating over object properties
+ * You can use a for...in loop or Object.keys(),
+ *  Object.values(), or Object.entries() methods to
+ * iterate over object properties.
+ */
+const obj4 = { a: 1, b: 2, c: 3 };
+
+for (let key in obj4) {
+  console.log(key, obj4[key]); // a 1, b 2, c 3
+}
+
+Object.keys(obj).forEach((key) => console.log(key, obj[key]));
+
+//using object.entires
+Object.entries(obj).forEach(([key, value]) => {
+  console.log(key, value);
+});
+
+/**
+ * 5 merging 2 objects
+ * You can merge two objects using Object.assign() or
+ *  the spread operator.
+ */
+
+const obj11 = { a: 1 };
+const obj22 = { b: 2 };
+
+const mergeObj = { ...obj11, ...obj22 };
+console.log(mergeObj);
+
+const mergeObj1 = Object.assign({}, obj22, obj11);
+console.log(mergeObj1);
+
+/**
+ * 6 deleting a property
  *
  */
-//length
-var a = [10, 20, 30];
-console.log(a.length);
+const obj12 = { a: 1, b: 2, c: 3 };
 
-var sh = [10, 20, 30, 40];
-console.log(sh.shift()); //remove the first arr element
-console.log(sh); //20,30,40
+delete obj12.b;
+console.log(obj12);
 
-var sha = [];
-console.log(sha.shift()); //undefined becuase arr is undefined
+/**
+ * 7 checking for empty object
+ *
+ *  How can you check if an object is empty
+ * (has no properties)?
+ */
+const object = {};
+const object1 = { a: 1 };
 
-//unshift - use to add element at the begining of array
+console.log(Object.keys(object).length === 0); //true
+console.log(Object.keys(object1).length === 0); //false
 
-console.log(sha.unshift("55"));
+/**
+ * defualt values for object properties
+ * undefined
+ */
+const newObj = { a: 1, b: undefined };
+
+const a = newObj.a || 10;
+console.log(a); //1
+const b = newObj.b || 20;
+console.log(b); //20
+
+// Using nullish coalescing operator (handles `undefined` and `null`)
+
+const c = newObj.c ?? 30;
+console.log(c); //30
