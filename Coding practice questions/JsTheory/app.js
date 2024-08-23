16caa9c3cb53aac84a7f114855d55bbb5e8cd0b6
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-23 16:25:40 +0530

    shallow copy and deep copy

--- Coding practice questions/JsTheory/app.js
+++ Coding practice questions/JsTheory/app.js
@@ -38,7 +38,37 @@ function validateCart() {
  * promise.race= first promise to settle whether it success or fail
  */
 
-
 /**
- * 
+ * shallow copy and deep copy:
+ *
+ * shallow copy create  new object butit still point to the same
+ * memory location
+ * it means if your making any changes to the new object
+ * it will reflect to the original object
+ *
+ * deep copy create a new object with new memeory location
+ * means it will create a new obejct and any changes make any in newq
+ * object it wont reflect to the original obj
+ *
+ *
  */
+
+let dev1 = {
+  name: "ruhci",
+  skills: {
+    primary: "js",
+  },
+};
+
+// let dev2 = dev1;
+// dev2.name = "abc";
+// console.log(dev2);
+
+// let dev2 = { ...dev1 };
+//with the spread operator we get the 1 level deep shallow copy
+// let dev2 = Object.assign({}, dev1);
+// let dev2 = JSON.parse(JSON.stringify(dev1));
+let dev2 = structuredClone(dev1);
+dev2.name = "abcdcjn";
+dev2.skills.primary = "raect";
+console.log(dev2);
