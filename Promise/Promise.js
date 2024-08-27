0037df4b1e07a5e0ab885fd55b4f1efd46ebfe2f
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-27 12:39:30 +0530

    polyfill for promise.any

--- Promise/Promise.js
+++ Promise/Promise.js
@@ -24,3 +24,29 @@ function mypromiseSettled(promiseArr) {
 
 Promise.allSettled([promise1, promise2]).then(console.log);
 mypromiseSettled([promise1, promise2]).then(console.log);
+
+//promise.any
+const promise11 = new Promise((res, rej) => setTimeout(rej, 3500, "data1"));
+const promise12 = new Promise((res, rej) => setTimeout(res, 1500, "data2"));
+
+function myPromiseAny(promiseArr) {
+  let errors = [];
+  return new Promise((res, rej) => {
+    promiseArr.forEach((promise, index) => {
+      promise
+        .then((data) => {
+          res(data);
+        })
+        .catch((error) => {
+          errors[index] = error;
+          if (index === promiseArr.length - 1) {
+            rej(new AggregateError(errors, "all promises were rejected"));
+          }
+        });
+    });
+  });
+}
+
+myPromiseAny([promise11, promise12])
+  .then(console.log)
+  .catch((err) => console.error(err.message));
