857cde9849dedd2726d243bad7d0914674214649
Ruchita Sagalgile <ruchitasagalgile04@gmail.com>
2024-08-27 11:11:46 +0530

    polyfill for promise.all

--- Promise/index.js
+++ Promise/index.js
@@ -11,3 +11,32 @@ function myPromiseRace(promiseArr) {
   });
 }
 myPromiseRace([promise1, promise2]).then(console.log);
+
+//Promise.all([])=>single promise
+
+const promise11 = new Promise((res, rej) => setTimeout(res, 100, "PROMISE11"));
+const promise22 = new Promise((res, rej) => setTimeout(res, 100, "promise22"));
+
+function mypromiseAll(promiseArr) {
+  let result = [];
+  return new Promise((res, rej) => {
+    promiseArr.forEach((promise, index) => {
+      promise
+        .then((data) => {
+          result[index] = data;
+          if (index === promiseArr.length - 1) {
+            res(result);
+          }
+        })
+        .catch((err) => rej(err));
+    });
+  });
+}
+
+mypromiseAll([promise11, promise22])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+Promise.all([promise11, promise22])
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
